9e85fd07659813289b4bcdbc7c35313baf77956b
Julia Sukach <35064670+JuliaSukach@users.noreply.github.com>
2025-01-05 16:16:32 +0100

    added navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Cart from './component/Cart';
 import ProductList from './component/ProductList';
+import Navigation from './component/Navigation';
 import './styles/fonts.css'
 
 function App() {
   return (
     <Provider store={store}>
       {/* <Cart/> */}
+      <Navigation/>
       <ProductList/>
     </Provider>
   );
